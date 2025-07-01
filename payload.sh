a4e998d3a8b6795ab457667e7351e652fadf1352
envoybughunter <envoybughunter@gmail.com>
2025-07-01 16:30:29 +0300

    Update payload.sh

--- payload.sh
+++ payload.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+echo "=== LOADED KERNEL MODULES ==="
+lsmod
+
+
 echo "=== RUNNING SERVICES ==="
 systemctl list-units --type=service --state=running
 
 
+echo "=== COMMAND INJECTION TEST ==="
+ls; echo "Injected command executed"
 
 
+echo "echo 'pwned' > /tmp/pwned.txt" > /tmp/test.sh
+chmod +x /tmp/test.sh
+/tmp/test.sh
+cat /tmp/pwned.txt
 
