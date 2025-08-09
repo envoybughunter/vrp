5bb2d978c051a62dfa8f2f7874d45c3c36f7c864
envoybughunter <envoybughunter@gmail.com>
2025-08-09 15:29:23 +0300

    Update payload.sh

--- payload.sh
+++ payload.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 SEARCH_DIR=${1:-/home}
 
-echo "Scanning $SEARCH_DIR for sensitive tokens..."
+echo "Searching $SEARCH_DIR for sensitive keys and secrets..."
 
 grep -r -E --color=auto \
-  'AKIA[0-9A-Z]{16}|AIza[0-9A-Za-z-_]{35}|ghp_[A-Za-z0-9]{36}' "$SEARCH_DIR" 2>/dev/null
+  '(AKIA[0-9A-Z]{16})|(AIza[0-9A-Za-z-_]{35})|(ghp_[A-Za-z0-9]{36})|(xox[baprs]-[0-9]{12}-[0-9]{12}-[a-zA-Z0-9]{24})|(sk_live_[0-9a-zA-Z]{24})|(eyJ[A-Za-z0-9-_]{10,}\.[A-Za-z0-9-_]{10,}\.[A-Za-z0-9-_]{10,})|(password|passwd|secret|token)[ ]*[:=][ ]*["'\''a-zA-Z0-9@#$%^&*_-]{8,}' "$SEARCH_DIR" 2>/dev/null
 
-echo "Scan finished."
+echo "Done."
