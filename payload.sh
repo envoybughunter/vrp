841870c354898fa5119b8bb84246d6660d7fa718
envoybughunter <envoybughunter@gmail.com>
2025-08-09 15:41:14 +0300

    Update payload.sh

--- payload.sh
+++ payload.sh
@@ -4,7 +4,5 @@ for dir in /tmp /var/tmp /dev/shm; do
   perms=$(stat -c "%a" "$dir")
   if [[ $perms == 777 ]]; then
     echo "World-writable directory found: $dir (permissions: $perms)"
-  else
-    echo "Directory $dir permissions are safe (permissions: $perms)"
   fi
 done
